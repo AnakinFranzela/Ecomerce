9febdd93e783f2866dd0bd4f022e028932559bd8
AnakinFranzela <v.d.yanchev@gmail.com>
2024-12-04 10:49:23 +0200

    Add project files.

---
+++ Ecomst/Areas/Admin/ViewModels/Category/CategoryViewModel.cs
@@ -0,0 +1,34 @@
+﻿using Ecomst.Entities;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Ecomst.Areas.Admin.ViewModels.Category
+{
+    public class CategoryViewModel
+    {
+        public int? Id { get; set; }
+
+        [Required(ErrorMessage = "Полето \"Име\" е задължително!")]
+        [DisplayName("Име")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Полето \"Ред на показване\" е задължително!")]
+        [DisplayName("Ред на показване")]
+        public int DisplayOrder { get; set; }
+
+        public void PopulateCategory(Entities.Category categoty)
+        {
+            categoty.Name = Name;
+            categoty.DisplayOrder = DisplayOrder;
+        }
+
+        public void PopulateFromCategory(Entities.Category? category)
+        {
+            if (category == null)
+                return;
+
+            Id = category.Id;
+            Name = category.Name;
+            DisplayOrder = category.DisplayOrder;
+        }
+    }
+}
---
+++ Ecomst/Areas/Admin/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ecomst.Areas.Admin.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
